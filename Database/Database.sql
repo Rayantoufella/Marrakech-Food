465d46906022f5aeefc545f43025bd46d9e861da
Rayan Toufella <rayantoufella3@gmail.com>
2026-04-09 11:01:06 +0100

    creation du base de donnee

---
+++ Database/Database.sql
@@ -0,0 +1,27 @@
+create database if not exists marrakech_food;
+
+use marrakech_food ;
+
+create table user (
+                      id int auto_increment primary key,
+                      name varchar(255) not null,
+                      email varchar(255) not null unique,
+                      password varchar(255) not null,
+                      created_at timestamp default current_timestamp
+) ;
+
+create table category (
+                          id int auto_increment primary key ,
+                          name varchar(255) not null
+) ;
+
+create table recette (
+                         id int auto_increment primary key ,
+                         name varchar(255) not null,
+                         description text not null,
+                         user_id int not null ,
+                         foreign key (user_id) references user(id) ,
+                         category_id int not null ,
+                         foreign key (category_id) references category(id)   ,
+                         created_at timestamp default current_timestamp
+) ;
